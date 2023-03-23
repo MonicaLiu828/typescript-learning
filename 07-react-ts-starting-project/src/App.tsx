cfa0793c840846058f7e275c24d33ef6feae1e1b
Monica Liu <liuqianmonica@gmail.com>
2023-03-23 15:30:40 -0700

    basic typescript learning

---
+++ 07-react-ts-starting-project/src/App.tsx
@@ -0,0 +1,10 @@
+import Todos from './components/Todos'
+function App() {
+  return (
+    <div>
+     <Todos items ={['learn react', 'learn typescript!']}/>
+    </div>
+  );
+}
+
+export default App;
