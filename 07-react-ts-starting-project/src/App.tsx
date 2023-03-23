77fd8d6d9e7d9c4dcb6c5350e635398ba9492e4b
Monica Liu <liuqianmonica@gmail.com>
2023-03-23 16:54:46 -0700

    add data model and component

--- 07-react-ts-starting-project/src/App.tsx
+++ 07-react-ts-starting-project/src/App.tsx
@@ -1,8 +1,16 @@
+
+import Todo from './models/todo'
 import Todos from './components/Todos'
 function App() {
+
+  const todos = [
+    new Todo('learning python'),
+    new Todo('learning react'),
+    new Todo('learning typescript'),
+  ]
   return (
     <div>
-     <Todos items ={['learn react', 'learn typescript!']}/>
+     <Todos items ={todos}/>
     </div>
   );
 }
